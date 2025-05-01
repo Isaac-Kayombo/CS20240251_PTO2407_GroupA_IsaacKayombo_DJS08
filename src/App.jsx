e2675c42164cd69d2237e1bfbf1b53f1b7a5ff50
IsaacK47 <isaackayombo41@gmail.com>
2025-05-01 13:00:39 +0200

    Adding navbar to App function

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,12 @@ function App() {
 
   return (
     <BrowserRouter>
-    <Link to="/">HOME</Link>
-    <Link to="/about">ABOUT</Link>
+       <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
