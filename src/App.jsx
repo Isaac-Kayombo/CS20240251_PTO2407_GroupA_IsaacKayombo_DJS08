96f8b21f6c102ad4c9eb6b2f9a5c5495ce975302
IsaacK47 <isaackayombo41@gmail.com>
2025-05-02 14:44:53 +0200

    Adding  host van content

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,10 @@ import Layout from './components/Layout'
 import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
-import HostLayout from './components/HostLayout'
 import HostVans from './pages/Host/HostVans'
+//import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostLayout from './components/HostLayout'
+
 import "./server"
 
 
---
+++ src/pages/Host/HostVansDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HostVansDetail() {
+    return (
+        <h1>The Host Vans Details</h1>
+    )
+}
+
+export default HostVansDetail
