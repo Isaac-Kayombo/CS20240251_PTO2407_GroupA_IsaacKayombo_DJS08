c86ed78af8a34dc6d1a0b9f3bad22ead63d9c9e8
IsaacK47 <isaackayombo41@gmail.com>
2025-05-03 14:29:50 +0200

    Setting Sad path loading state

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -6,13 +6,16 @@ function Vans() {
 
     const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
 
     const typeFilter = searchParams.get("type")
     
     React.useEffect(() => {
         async function loadVans() {
+            setLoading(true)
             const data = await getVans()
             setVans(data)
+            setLoading(false)
         }
         
         loadVans()
@@ -44,6 +47,10 @@ function Vans() {
             }
             return prevParams
         })
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
     }
 
     return (
