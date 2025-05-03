1cc647eea32d945d5c468334ca5181aa5adce905
IsaacK47 <isaackayombo41@gmail.com>
2025-05-03 12:23:24 +0200

    Fixing absolute paths

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -20,7 +20,7 @@ function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-           <Link to={`/vans/${van.id}`} >
+           <Link to={van.id}>
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -73,7 +73,7 @@ function Vans() {
                 ) : null}
 
             </div>
-            
+
             <div className="van-list">
                 {vanElements}
             </div>
