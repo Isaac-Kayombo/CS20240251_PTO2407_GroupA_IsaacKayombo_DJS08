f9ea7b54205c45abb4591ec47b6dfe43b333dbcc
IsaacK47 <isaackayombo41@gmail.com>
2025-05-03 13:25:05 +0200

    Adding 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import HostVansDetail from "./pages/Host/HostVansDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import HostVanPricing from "./pages/Host/HostVanPricing"
+import NotFound from './pages/NotFound'
 import HostLayout from './components/HostLayout'
 
 import "./server"
@@ -54,6 +55,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path='*' element={<NotFound />}/>
         </Route>
       </Routes>
     </BrowserRouter>
